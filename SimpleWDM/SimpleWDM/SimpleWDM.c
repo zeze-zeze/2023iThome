8efd9f5f9ca82603f30820e2b33e979770d71b7f
zeze <zezectf@gmail.com>
2023-09-17 00:42:10 +0800

    feat: SimpleWDM

---
+++ SimpleWDM/SimpleWDM/SimpleWDM.c
@@ -0,0 +1,18 @@
+﻿#include <ntddk.h>
+
+void DriverUnload(PDRIVER_OBJECT db)
+{
+    UNREFERENCED_PARAMETER(db);
+}
+
+extern NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+{
+    UNREFERENCED_PARAMETER(DriverObject);
+    UNREFERENCED_PARAMETER(RegistryPath);
+
+    // 要設定卸載函數驅動程式才能順利卸載
+    DriverObject->DriverUnload = DriverUnload;
+    DbgPrint("Hello World\n");
+
+    return STATUS_SUCCESS;
+}
