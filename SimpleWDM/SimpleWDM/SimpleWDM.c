8efb78d4df3b77e4725c90eb993bd7271df5dfc5
zeze <zezectf@gmail.com>
2023-09-17 00:56:09 +0800

    docs: add comment int SimpleWDM

--- SimpleWDM/SimpleWDM/SimpleWDM.c
+++ SimpleWDM/SimpleWDM/SimpleWDM.c
@@ -12,6 +12,8 @@ extern NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING Registr
 
     // 要設定卸載函數驅動程式才能順利卸載
     DriverObject->DriverUnload = DriverUnload;
+
+    // 用 DbgPrint 印出 Hello World，可以在 DbgView 工具中觀察
     DbgPrint("Hello World\n");
 
     return STATUS_SUCCESS;
